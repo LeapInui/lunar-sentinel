12ce2b7cdd6304611bfea2b0ba4b9aef77544b23
LeapInui <j05huayau.29@gmail.com>
2025-03-13 10:12:24 +0000

    code cleanup

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
     [SerializeField] private SpriteRenderer spriteRenderer;
 
     public void RotateGun(Vector2 targetPosition)
     {
-        Vector2 direction = (targetPosition - (Vector2)target.position).normalized;
+        Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        target.rotation = Quaternion.Euler(0, 0, angle);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
 
-        spriteRenderer.flipY = targetPosition.x < target.position.x;
+        spriteRenderer.flipY = targetPosition.x < transform.position.x;
     }
 }
